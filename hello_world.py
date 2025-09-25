925d664ee75843c2f2843d31f3774e943647f37e
anthonykirkness <anthonykirkness@gmail.com>
2025-09-25 17:08:43 +0000

    Hello there

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,14 @@
+print("hello world ")
 
+import array as arr
+
+a = arr. array(
+    'h', # opcode for usigned short
+    [26] # list
+)
+
+b = arr.array ('l', [1, 2, 3, 4, 5])
+
+print(a.pop())
+
+print(b.pop())
